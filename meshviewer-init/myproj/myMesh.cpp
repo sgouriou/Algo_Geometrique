4c6fdcf3a13db0075a2fdf2944f7cbeaf865387c
sgouriou <gouriou.stephen@gmail.com>
2017-01-12 00:25:20 +0100

    Finished MyMesh::readFile Function

--- meshviewer-init/myproj/myMesh.cpp
+++ meshviewer-init/myproj/myMesh.cpp
@@ -36,15 +36,79 @@ void myMesh::clear()
 
 void myMesh::checkMesh()
 {
-	vector<myHalfedge *>::iterator it;
-	for (it = halfedges.begin(); it != halfedges.end(); it++)
+	bool error;
+	unsigned int count;
+
+	cout << "Checking mesh for errors...\n";
+	cout << "\tNumber of vertices: " << vertices.size() << endl;
+	cout << "\tNumber of halfedges: " << halfedges.size() << endl;
+	cout << "\tNumber of faces: " << faces.size() << endl << endl;
+
+	cout << "\tChecking for NULL vertices. " << endl;
+	for (unsigned int i = 0; i < vertices.size(); i++)
 	{
-		if ((*it)->twin == NULL)
-			break;
+		if (vertices[i] == NULL)
+			cout << "\t\tError: vertex " << i << " is NULL.\n";
+		else if (vertices[i]->originof == NULL)
+			cout << "\t\tError: the originof halfedge of vertex " << i << " is NULL.\n";
 	}
-	if (it != halfedges.end())
-		cout << "Error! Not all edges have their twins!\n";
-	else cout << "Each edge has a twin!\n";
+	cout << "\t  Ended check.\n\n";
+
+	cout << "\tChecking the halfedges. " << endl;
+	error = false;
+	count = 0;
+	for (unsigned int i = 0; i < halfedges.size(); i++)
+	{
+		if (halfedges[i]->source == NULL)
+			cout << "\tError: Source is NULL for halfedge " << i << endl;
+		if (halfedges[i]->twin == NULL) count++;
+		if (halfedges[i]->next == NULL || halfedges[i]->prev == NULL)
+			cout << "\tError: Next/prev NULL for halfedge " << i << endl;
+		if (halfedges[i]->next->prev != halfedges[i] || halfedges[i]->prev->next != halfedges[i])
+			cout << "\tError: Next/prev not set properly for halfedge " << i << endl;
+		if (halfedges[i]->twin != NULL && halfedges[i] != halfedges[i]->twin->twin)
+			cout << "\tError: Twin pair not set properly for halfedge " << i << endl;
+	}
+	if (count > 0) cout << "\tThis mesh has boundary edges.\n";
+	cout << "\t  Ended check.\n\n";
+
+	cout << "\tChecking fans of each vertex.\n";
+	for (unsigned int i = 0; i<vertices.size(); i++) {
+		myVertex *v = vertices[i];
+
+		myHalfedge *e1 = v->originof;
+
+		unsigned int k = 0;
+		do {
+			k++;
+			e1 = e1->prev->twin;
+			if (k > 100000) cout << "\t\tError: Infinite loop when checking adjacent edges for vertex " << i << endl;
+		} while (e1 != NULL && e1 != v->originof);
+	}
+	cout << "\t  Ended check.\n\n";
+
+	cout << "\tChecking edges of each face.\n";
+	unsigned int num_incidentedgesoverallfaces = 0;
+	bool istriangular = true;
+	for (unsigned i = 0; i<faces.size(); i++) {
+		myHalfedge *e1 = faces[i]->adjacent_halfedge;
+		unsigned int k = 0;
+		do {
+			k++;
+			if (e1 == NULL) cout << "\t\tError: Found NULL edge on boundary of face " << i << endl;
+			e1 = e1->next;
+			if (k > 100000) cout << "\t\tError: Infinite loop when checking adjacent edges for face " << i << endl;
+		} while (e1 != faces[i]->adjacent_halfedge);
+		num_incidentedgesoverallfaces += k;
+		if (k>3) istriangular = false;
+	}
+	if (istriangular) cout << "\t\tThe mesh is triangular.\n";
+	else cout << "\t\tThe mesh is not triangular.\n";
+	if (num_incidentedgesoverallfaces != halfedges.size())
+		cout << "\t\tSuspicious: the total number of halfedges is not equal to the sum at each face.\n";
+	cout << "\t  Ended check.\n\n";
+
+	cout << "  Ended check of mesh.\n";
 }
 
 
@@ -103,17 +167,17 @@ bool myMesh::readFile(std::string filename)
 			//while (myline >> u) cout << atoi((u.substr(0, u.find("/"))).c_str()) << " ";
 			//cout << endl;
 
-			myFace* face = new myFace();
-			//vector<int> vertexIndicesofFace;
+			myFace* currentFace = new myFace();
 			
-			vector<int> faceIndices;
+			
+			vector<int> vertexIndicesofFace;
 			vector<myHalfedge*> tempHalfedges;
 			
 			string tempStr;
 			while(myline >> tempStr)
 			{
-				
-				//vertexIndicesofFace.push_back(stoi(tempStr));
+		
+				vertexIndicesofFace.push_back(stoi(tempStr));
 				tempHalfedges.push_back(new myHalfedge());
 			}
 			
@@ -125,13 +189,41 @@ bool myMesh::readFile(std::string filename)
 				int prev = (i - 1 + tempHalfedges.size()) % tempHalfedges.size();
 
 
+				//Initializing the connection between Vertex and Hedge.
+				tempHalfedges[i]->source = vertices[vertexIndicesofFace[i]];
+				vertices[vertexIndicesofFace[i]]->originof = tempHalfedges[i];
 
+				tempHalfedges[i]->next = tempHalfedges[next];
+				tempHalfedges[i]->prev = tempHalfedges[prev];
+				tempHalfedges[i]->adjacent_face = currentFace;
+
+				
+				// pushing new halfEgde
+				halfedges.push_back(tempHalfedges[i]);
+
+				// now getting to the mapping part
+				twin_map[make_pair(vertexIndicesofFace[i], vertexIndicesofFace[next])] = tempHalfedges[i];
+				it = twin_map.find(make_pair(vertexIndicesofFace[next], vertexIndicesofFace[i]));
+
+				if (it != twin_map.end())//if we found a twin candidate
+				{
+					it->second->twin = tempHalfedges[i];
+					tempHalfedges[i]->twin = it->second;
+
+				}
+				
+				currentFace->adjacent_halfedge = tempHalfedges[0];
+				faces.push_back(currentFace);
 
 
 			}
 
 
 		}
+
+		checkMesh();
+
+		return true;
 	}
 
 	checkMesh();
